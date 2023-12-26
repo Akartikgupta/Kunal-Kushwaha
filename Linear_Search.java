7d75c8ee1d2a7caab7abc04594811588bfcaa562
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2023-12-26 23:26:17 +0530

    Add files via upload

---
+++ Linear_Search.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Linear_Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 3, 4, 2, 5, 6 };
+        for (int i = 0; i < arr.length; i++) {
+            int search = sc.nextInt();
+            if (arr[i] == search) {
+                System.out.println("Element found at this index" + i + "and match value is " + search);
+            } else {
+                System.out.println("Not found a similar match with the element");
+            }
+
+        }
+    }
+}
