0a2581318243641d2dec047b745482d158cff1ee
Kartik Gupta <134687458+Akartikgupta@users.noreply.github.com>
2023-12-30 23:36:20 +0530

    Add files via upload

---
+++ Reverse_Array.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Reverse_Array {
+    public static void main(String[] args) {
+        int arr[] = { 10, 20, 22, 30, 40, 50 };
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+}
